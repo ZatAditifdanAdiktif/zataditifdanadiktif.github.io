8885174b26018cdf81b4dcd1fe4be75c33d8b40e
ZatAditifdanAdiktif <93830666+ZatAditifdanAdiktif@users.noreply.github.com>
2022-01-22 23:03:00 +0800

    publish4

--- bahan/zat adiktif/Kuis/soal.js
+++ bahan/zat adiktif/Kuis/soal.js
@@ -2,14 +2,21 @@
 
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     var firebaseConfig = {
-        apiKey: "AIzaSyAXTrIuXO4OGau7EliaUj6XE-7OS0FIN8Q",
-        authDomain: "percobaan-3aa8e.firebaseapp.com",
-        databaseURL: "https://percobaan-3aa8e-default-rtdb.firebaseio.com",
-        projectId: "percobaan-3aa8e",
-        storageBucket: "percobaan-3aa8e.appspot.com",
-        messagingSenderId: "51589690048",
-        appId: "1:51589690048:web:e63a8de81c95edc3d7f325",
-        measurementId: "G-D8GZMGNJ69"
+        apiKey: "AIzaSyDQ3M56ogy44NUdFR9R06ssu22O1_LpvSM",
+
+        authDomain: "skripsi-b6ace.firebaseapp.com",
+    
+        projectId: "skripsi-b6ace",
+    
+        storageBucket: "skripsi-b6ace.appspot.com",
+    
+        messagingSenderId: "788043031718",
+    
+        appId: "1:788043031718:web:64005851b8d5cee5a04067",
+    
+        measurementId: "G-RVBX66JYWL"
+
+
         };
         // Initialize Firebase
         firebase.initializeApp(firebaseConfig);
