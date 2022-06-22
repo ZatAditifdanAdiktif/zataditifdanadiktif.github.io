24c91783f090fa9105ee8e599bca8ae78cde65cd
ZatAditifdanAdiktif <93830666+ZatAditifdanAdiktif@users.noreply.github.com>
2022-06-23 01:08:03 +0800

    Add files via upload

---
+++ zat aditif/1.js
@@ -0,0 +1,62 @@
+
+		function pertanyaan1(pilihan) {
+			if (pilihan==1) {
+				document.getElementById("jawaban1").innerHTML = "Maaf jawaban anda kurang tepat";
+				document.getElementById("jawaban1").style.color="red";
+			}
+			else{
+				document.getElementById("jawaban1").innerHTML = "Tepat sekali, Karena mengandung bahan bersifat racun bagi yang mengkonsumsinya";
+				document.getElementById("jawaban1").style.color="green";
+			}
+		}
+	
+	
+	
+		function pertanyaan2(pilihan) {
+			if (pilihan==1) {
+				document.getElementById("jawaban2").innerHTML = "Maaf. Jawaban anda kurang tepat" ;
+				document.getElementById("jawaban2").style.color="red" ;
+			}
+			else{
+				document.getElementById("jawaban2").innerHTML = "Tepat sekali, Yang disarankan adalah pewarna alami, karena tidak memiliki efek samping" ;
+				document.getElementById("jawaban2").style.color = "green" ;
+			}
+		}
+	
+
+	
+		function pertanyaan3(pilihan) {
+			if (pilihan==1) {
+				document.getElementById("jawaban3").innerHTML = "Benar sekali, Kunyit digunakan memberi warna kuning pada makanan" ;
+				document.getElementById("jawaban3").style.color="green" ;
+			}
+			else{
+				document.getElementById("jawaban3").innerHTML = "Maaf, Jawaban anda belum tepat" ;
+				document.getElementById("jawaban3").style.color = "red" ;
+			}
+		}
+	
+	
+		function pertanyaan4(pilihan) {
+			if (pilihan==1) {
+				document.getElementById("jawaban4").innerHTML = "Benar sekali, Tujuan dari pengawet untuk mencegah reaksi kimia tertentu pada makanan" ;
+				document.getElementById("jawaban4").style.color="green" ;
+			}
+			else{
+				document.getElementById("jawaban4").innerHTML = "Maaf, Jawaban anda belum tepat" ;
+				document.getElementById("jawaban4").style.color= "red" ;
+			}
+		}
+	
+	
+		function pertanyaan5(pilihan) {
+			if (pilihan==1) {
+				document.getElementById("jawaban5").innerHTML = "Benar sekali, Daun pandan adalah zat aditif alamiah untuk memberikan warna hijau" ;
+				document.getElementById("jawaban5").style.color = "green" ;
+			}
+			else{
+				document.getElementById("jawaban5").innerHTML = "Maaf, Jawaban anda belum tepat" ;
+				document.getElementById("jawaban5").style.color="red";
+			}
+		}
+	
